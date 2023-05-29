46bf5fbd10a416d537c5359299abe3e362c6a31f
Knyazev Vitaly <13.kn.v.v@gmail.com>
2023-05-29 18:50:06 +0300

    add card

--- src/Components/Cards/Cards.tsx
+++ src/Components/Cards/Cards.tsx
@@ -1,13 +1,45 @@
 import s from './Cards.module.scss';
+import logo from '../../assets/images/S7Logo.svg';
 
 function Cards() {
   return (
     <ul className={s.Cards}>
-      <li className={s.Card}>card</li>
-      <li className={s.Card}>card</li>
-      <li className={s.Card}>card</li>
-      <li className={s.Card}>card</li>
-      <li className={s.Card}>card</li>
+      {[1, 2, 3, 4, 5].map(() => (
+        <li className={s.Card}>
+          <header className={s.header}>
+            <h2 className={s.price}>13 400 r</h2>
+            <img src={logo} alt="s7" />
+          </header>
+          <ul className={s.flight}>
+            <li>
+              <h3 className={s.flightSubTitle}>MOW – HKT</h3>
+              <div className={s.flightInfo}>10:45 – 08:00</div>
+            </li>
+            <li>
+              <h3 className={s.flightSubTitle}>MOW – HKT</h3>
+              <div className={s.flightInfo}>10:45 – 08:00</div>
+            </li>
+            <li>
+              <h3 className={s.flightSubTitle}>MOW – HKT</h3>
+              <div className={s.flightInfo}>10:45 – 08:00</div>
+            </li>
+          </ul>
+          <ul className={s.flight}>
+            <li>
+              <h3 className={s.flightSubTitle}>MOW – HKT</h3>
+              <div className={s.flightInfo}>10:45 – 08:00</div>
+            </li>
+            <li>
+              <h3 className={s.flightSubTitle}>MOW – HKT</h3>
+              <div className={s.flightInfo}>10:45 – 08:00</div>
+            </li>
+            <li>
+              <h3 className={s.flightSubTitle}>MOW – HKT</h3>
+              <div className={s.flightInfo}>10:45 – 08:00</div>
+            </li>
+          </ul>
+        </li>
+      ))}
     </ul>
   );
 }
